3d8b28c21e9b9184b46cdab1c8b352440924d53a
ramajorgie <ramaandikajorgie@gmail.com>
2021-04-13 23:12:34 +0700

    update_about

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -73,6 +73,7 @@ class PostController extends Controller
 
     public function view_about()
     {   
+        
         $data = DB::table('post_about')->get();
         return view('page_layout.form_postabout',['data'=>$data]);
     }
