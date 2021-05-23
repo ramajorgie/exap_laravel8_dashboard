ba8e72bb897f7e776d9831116f2b995a77836a72
ramajorgie <ramaandikajorgie@gmail.com>
2021-05-23 20:41:16 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -99,6 +99,8 @@
                             </div>
                         </div>
                     </div>
+
+                    
                 </div>
 
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
@@ -121,6 +123,7 @@
                             </a>
                         </div>
                     </div>
+                    
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
